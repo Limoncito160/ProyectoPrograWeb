cf1ca12d0a82927d0102fddaa4926e758e8b6400
RSLzn21 <edkorn_117@hotmail.com>
2023-05-10 22:43:53 -0600

    Subi archivos

---
+++ Proyecto/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar Sesión</title>
+    <link rel="stylesheet" href="css/slog.css">
+</head>
+<body>
+
+<h2>Iniciar Sesión</h2>
+
+<form action="/action_page.php" method="post">
+<div class="imgcontainer">
+    <img src="img/avatar.png" alt="Avatar" class="avatar">
+</div>
+
+<div class="container">
+    <!--Etiqueta y Caja de texto de Correo-->
+    <label for="uname"><b>Correo Electronico</b></label>
+    <input type="text" placeholder="Correo@ejemplo.com" name="uname" required>
+    <!--Etiqueta y Caja de texto de Contraseña-->
+    <label for="psw"><b>Contraseña</b></label>
+    <input type="password" placeholder="***************" name="psw" required>
+    <!--Boton Acceso-->
+    <button type="submit">Acceso</button>
+</div>
+    <!--¿No Tienes Cuenta?-->
+    <span class="psw">¿No Tienes Cuenta? <a href="registro.php">¡Crea Una!</a></span>
+</form>
+
+</body>
+</html>
---
+++ Proyecto/registro.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+    <title>Registro</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container my-5">
+    <h1 style="text-align: center;">Registro de Usuario</h1>
+
+    <form method="post">
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Nombres: </label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="nombres">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Apellido Paterno: </label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="ap_paterno">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Apellido Materno: </label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="ap_materno">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Fecha de Nacimiento: </label>
+                <div class="col-sm-6">
+                    <input type="date" class="form-control" name="f_nacimiento">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Telefono: </label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="telefono">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Correo Electronico: </label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="email">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Contraseña: </label>
+                <div class="col-sm-6">
+                    <input type="password" class="form-control" name="password">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <div class="offset-sm-3 col-sm-3 d-grid">
+                    <button type="submit" class="btn btn-primary">Agregar</button>
+                </div>
+                <div class="offset-sm-3 col-sm-3 d-grid">
+                    <a class="btn btn-outline-primary" href="login.php" role="button">Cancelar<a>
+                </div>
+            </div>
+    </form>
+    </div>
+</body>
+</html>
