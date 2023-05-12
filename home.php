12058ce604d818162e69c9e0cd9431675a041b02
RSLzn21 <edkorn_117@hotmail.com>
2023-05-11 21:43:05 -0600

    He editado el archivo nota.txt

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 include("header.php");
 ?>
 <div class="container" style="background-color: lightblue;">
-  <h3 style="text-align:left; color:lightcoral;" >Nombre de la empresa</h3>
+  <h3 style="text-align:left; color:lightcoral;" > <img src="img/arte.jpg" height="180px" width="180px"> Nombre de la empresa</h3>
   <h4>Mision</h4>
   <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil tenetur voluptatem rem nobis mollitia accusamus quibusdam odit quae nostrum quod possimus magnam amet a culpa, earum pariatur quas eveniet ducimus.</p>
   <h5>Vision</h5>
