1c03e61390d99b18337f0b3f946e0f669651708c
RSLzn21 <edkorn_117@hotmail.com>
2023-05-14 18:33:53 -0600

    He editado el archivo nota.txt

--- detalles_producto.php
+++ detalles_producto.php
@@ -3,5 +3,24 @@ include("header.php");
 ?>
 <div class="container" style="background-color: lightblue;">
   <h3 style="text-align:left; color:lightcoral;">Detalles del Producto</h3>
-  <!--Inserte codigo tabla-->
+  <br>
+  <div>
+    <div style="float: left">
+      <img src="img/arte.jpg" alt="Imagen del Producto" width="200px" height="200px">
+    </div>
+    <div>
+      <p>Articulo</p>
+      <p>$</p>
+      <p>Proveedor</p>
+    </div>
+  </div>
+<br>
+<div class="row mb-3">
+  <div class="offset-sm-3 col-sm-3 d-grid">
+    <button type="submit" class="btn btn-primary" style="background-color: orangered;">Agregar al carrito</button>
+  </div>
+  <div class="offset-sm-3 col-sm-3 d-grid">
+    <a class="btn btn-outline-primary" href="home.php" role="button">Regresar<a>
+  </div>
+  </div>
 </div>
