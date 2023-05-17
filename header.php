2cdb52867f82cab0d74ced870abbbd6314c59395
RSLzn21 <edkorn_117@hotmail.com>
2023-05-17 15:59:09 -0600

    Lzn edito header.php

--- header.php
+++ header.php
@@ -14,16 +14,16 @@
 
 <body>
   <!--Barra de navegación-->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: darkgray; display:flex;">
-    <p class="navbar-brand disable">Pincheles S.A. de C.V.</p>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color:black; display:flex;">
+    <p class="navbar-brand disable" style="color: white;">Pincheles S.A. de C.V.</p>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="home.php"><span class="glyphicon glyphicon-home"></span> Pagina Principal</a></li>
-      <li><a href="articulos.php">Articulos</a></li>
-      <li><a href="pedidos.php">Pedidos</a></li>
+      <li class="active"><a href="home.php" style="color: white;"><span class="glyphicon glyphicon-home"></span> Pagina Principal</a></li>
+      <li><a href="articulos.php" style="color: white;">Articulos</a></li>
+      <li><a href="pedidos.php" style="color: white;">Pedidos</a></li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
           aria-haspopup="true" aria-expanded="false">
-          <span class="glyphicon glyphicon-earphone"> Contacto
+          <span class="glyphicon glyphicon-earphone" style="color: white;"> Contacto
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <p>Telefono: </p><a class="dropdown-item" href="#">722 554 5526</a>
@@ -32,13 +32,10 @@
 
       </li>
     </ul>
-    <u1 class="nav navbar-nav">
-
+    <u1 class="nav navbar-nav" style="margin-left: 615px;">
+    <li><a href="login.php" style="color: white;">Cerrar Sesión</a></li>
     </u1>
-    <form method="POST" action="login.php">
-    <input type="submit" name="logout" value="Cerrar sesión" style="margin-top:15px; margin-left:690px;">
-</form>
-
+    
 <?php
 if (isset($_POST['logout'])) {
     session_unset();
@@ -50,3 +47,4 @@ if (isset($_POST['logout'])) {
 ?>
 
   </nav>
+</body>
